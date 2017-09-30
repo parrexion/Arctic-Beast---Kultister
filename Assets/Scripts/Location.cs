abba99b13f052c651633cae38fe8bc37b8718091
Crouchmode <maak0017@student.umu.se>
2017-09-30 14:11:20 +0200

    World map classes
    World map classes

---
+++ Assets/Scripts/Location.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class Location : ScriptableObject {
+
+	public string name;
+	public double explorationRate = 0;
+
+	public double encounterRisk() {
+		return 1 - (1 / (1 + explorationRate));
+	}
+}
+
---
+++ Assets/Scripts/Path.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Path : ScriptableObject {
+	public double explorationRate = 0;
+	public double terrainCost;
+
+	public double travelCost() {
+		double exp = 1 / (1 + explorationRate);
+		return terrainCost*(1-exp/2);
+	}
+}
+
