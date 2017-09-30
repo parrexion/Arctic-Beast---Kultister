288c2647222c2ea617244c720ce95f5d043a7186
Parrexion <schack-93@hotmail.com>
2017-09-30 22:53:32 +0200

    Random paths

--- Assets/Scripts/WorldMap/MapPanelSetup.cs
+++ Assets/Scripts/WorldMap/MapPanelSetup.cs
@@ -8,32 +8,60 @@ public class MapPanelSetup : MonoBehaviour {
 	private WorldMap map;
 
 	public Transform locationContainer = null;
+	public Transform pathContainer = null;
 	public Transform baseCamp = null;
-	public GameObject LocationPrefab = null;
 
-	public float mapHeight = 90;
-	public float mapWidth = 90;
+	public GameObject LocationPrefab = null;
+	public GameObject pathPrefab = null;
+
+	public float mapHeight = 30;
+	public float mapWidth = 30;
 
 	// Use this for initialization
 	void Start () {
 		map = GetComponent<WorldMap>();
+		map.ShufflePaths();
+		RectTransform rect = GetComponent<RectTransform>();
+		float branchesSize = (float)map.randomPaths.Length+1;
+		float deviationRange = Mathf.PI / (branchesSize * 3);
 
-		for (int branch = 0; branch < map.paths.Count; branch++) {
-			Path p = map.paths[branch];
+		for (int branch = 0; branch < map.randomPaths.Length; branch++) {
+			Path p = map.randomPaths[branch];
 			Location location = baseCamp.GetComponent<LocationUI>().locationValues;
+			Vector3 position = baseCamp.transform.position;
+
+			mapWidth = rect.rect.width / 8f;
+			mapHeight = rect.rect.height / (p.locationsOnPath.Length+1);
+
 			for (int i = 0; i < p.locationsOnPath.Length; i++) {
 				int cost = map.GetRandomTravelCost();
 				Location nextLocation = p.locationsOnPath[i];
 				nextLocation.travelCost = cost;
-				location.exits.Add(nextLocation);
-				location = nextLocation;
 
 				GameObject createdLocation = GameObject.Instantiate(LocationPrefab);
 				createdLocation.transform.SetParent(locationContainer);
-				createdLocation.transform.position = 
-							new Vector3((i+1) *mapWidth * Mathf.Cos((branch+1)*Mathf.PI/5f),
-										(i+1) *mapHeight * Mathf.Sin((branch+1)*Mathf.PI/5f), 0);
-				Debug.Log("Transfor: " + createdLocation.transform.position);
+
+				float angle = (branch+1)*Mathf.PI/branchesSize;
+				angle += Random.Range(-deviationRange,deviationRange);
+				createdLocation.transform.localPosition = 
+							new Vector3((i+1) *mapWidth * Mathf.Cos(angle),
+										(i+1) *mapHeight * Mathf.Sin(angle), 0);
+				float distance = Vector3.Distance(position,createdLocation.transform.position) / 100f;
+				Debug.Log("Distance: " + distance);
+				Vector3 dir = createdLocation.transform.position - position;
+				Debug.Log("dir: " + dir);
+				angle = Mathf.Atan2(dir.y, dir.x) *Mathf.Rad2Deg;
+
+				GameObject createdPaths = GameObject.Instantiate(pathPrefab);
+				createdPaths.transform.SetParent(pathContainer);
+				createdPaths.transform.position = position;
+				createdPaths.transform.localScale = new Vector3(distance,1,1);
+
+				createdPaths.transform.Rotate(new Vector3(0,0,angle));
+
+				location.exits.Add(nextLocation);
+				location = nextLocation;
+				position = createdLocation.transform.position;
 			}
 		}
 	}
--- Assets/Scripts/WorldMap/WorldMap.cs
+++ Assets/Scripts/WorldMap/WorldMap.cs
@@ -7,6 +7,19 @@ public class WorldMap : MonoBehaviour {
 	public int startExits = 3;
 	public List<Path> paths;
 	public List<int> pathCosts;
+	public Path[] randomPaths;
+
+
+	public void ShufflePaths(){
+		randomPaths = new Path[paths.Count];
+		int i = 0;
+		while (paths.Count > 0) {
+			int r = Random.Range(0,paths.Count);
+			randomPaths[i] = paths[r];
+			paths.RemoveAt(r);
+			i++;
+		}
+	}
 
 
 	public int GetRandomTravelCost(){
