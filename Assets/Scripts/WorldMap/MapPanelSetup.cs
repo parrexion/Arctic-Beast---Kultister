ee54e1ac72477334d3aa132c8a15271e92a7a9d4
Crouchmode <maak0017@student.umu.se>
2017-09-30 15:57:34 +0200

    Init of map screen
    Big script initializing map screen to match a WorldMap object

---
+++ Assets/Scripts/WorldMap/MapPanelSetup.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapPanelSetup : MonoBehaviour {
+
+	public WorldMap map;
+
+	public GameObject locationContainer;
+	public GameObject locationPrefab;
+	public float mapHeight = 45;
+	public float mapWidth = 45;
+
+	// Use this for initialization
+	void Start () {
+		List<Path> currentLevel = map.baseExits;
+		List<Path> nextLevel = new List<Path>();
+		List<List<Path>> levels = new List<List<Path>>();
+		var locationsLeft = true;
+		while (currentLevel.Count > 0) {
+			foreach (Path p in currentLevel) {
+				nextLevel.AddRange(p.destination.exits);
+
+			}
+			levels.Add(currentLevel);
+			currentLevel = nextLevel;
+			nextLevel = new List<Path>();
+		}
+
+		int level = 1;
+		foreach(List<Path> l in levels) {
+			int location = 0;
+			foreach (Path p in l) {
+				Transform trans = GameObject
+					.Instantiate(locationPrefab)
+					.GetComponent<Transform>();
+
+				trans.Translate(new Vector3(mapWidth/location, mapHeight/level, 0f));
+				++location;
+			}
+			++level;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
