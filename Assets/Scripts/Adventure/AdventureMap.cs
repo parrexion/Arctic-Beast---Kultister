472d8c28609e087c023b5b5bbcc2c14e806fcbab
Robin Ekman <robin.seth.ekman@gmail.com>
2017-09-30 14:42:01 +0200

    Made classes for AdventureMap etc.

---
+++ Assets/Scripts/Adventure/AdventureMap.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdventureMap : MonoBehaviour {
+    public AdventureTile[][] tiles;
+    public PlayerActor player;
+    public List<NPActor> npcs;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Adventure/AdventureTile.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public enum TerrainType {
+    Snow, //Basic terrain -- walkable by everyone
+    Water, //Walkable by swimmers
+    Rock, //Blocked, but doesn't block sight.
+    BigRock, //Blocks walking, blocks sight.
+    Tree,
+};
+
+public abstract class AdventureTile : ScriptableObject {
+
+    // Use this for initialization
+    public Sprite graphic;
+    //public string name;
+    public bool blocksSight;
+    public TerrainType terrainType;
+    public List<GameItem> pickups;
+
+    bool TryWalk(Actor walker)
+    {
+        return walker.walkableTerrains.Contains(this.terrainType);
+    }
+    public abstract void OnEnter();
+    public abstract void OnExit();
+    public abstract void OnTick();
+
+}
