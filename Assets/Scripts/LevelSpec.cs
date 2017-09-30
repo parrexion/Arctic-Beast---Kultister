36f1973016a35164686282c0eebb3ea9c44ce234
Crouchmode <maak0017@student.umu.se>
2017-10-01 00:11:56 +0200

    Global oobjects caries level variable

---
+++ Assets/Scripts/LevelSpec.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSpec : MonoBehaviour {
+
+	public int width = 4;
+	public int height = 4;
+	public int goalCount = 3;
+	public int correctness = 4;
+	public AdventureTile challangeTile;
+	public List<NPActor> npcs;
+}
