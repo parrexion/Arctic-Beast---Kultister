5ef736758c743f935c5d836d54b8920ea8a2253e
Crouchmode <maak0017@student.umu.se>
2017-10-01 01:01:50 +0200

    Makes singletone

--- Assets/Scripts/LevelSpec.cs
+++ Assets/Scripts/LevelSpec.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class LevelSpec : MonoBehaviour {
 
+	#region Singleton
+	public static LevelSpec instance = null;
+	void Awake(){
+		if (instance != null)
+			Destroy(gameObject);
+		else
+			instance = this;
+	}
+	#endregion
+
 	public int width = 4;
 	public int height = 4;
 	public int goalCount = 3;
