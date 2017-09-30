0f7b677e0f93f2a99d625211f7b8991ec476f75a
Robin Ekman <robin.seth.ekman@gmail.com>
2017-09-30 18:36:12 +0200

    Progress on walk. Need to set null actions.

--- Assets/Scripts/Adventure/Actor.cs
+++ Assets/Scripts/Adventure/Actor.cs
@@ -6,19 +6,20 @@ public class Actor : MonoBehaviour {
     public List<TerrainType> walkableTerrains ;
     // Use this for initialization
 
-    public XYCoordinate coordinates;
+    public int x, y;
     public AdventureMap parentMap;
 
     public void initPlayer(int x, int y, AdventureMap parentMap)
     {
-        this.coordinates = new XYCoordinate(x, y);
+        this.x = x;
+        this.y = y;
         this.parentMap = parentMap;
     }
 
     public bool walk(Direction wd)
     {
-        int x = this.coordinates.x;
-        int y = this.coordinates.y;
+        int x = this.x;
+        int y = this.y;
         switch (wd)
         {
             case Direction.North:
@@ -33,19 +34,15 @@ public class Actor : MonoBehaviour {
                 break;
 
         }
-        Debug.Log(string.Format("Trying to walk. {0} {1} {2}", this.parentMap.tiles.Count, x, y));
-        foreach( XYCoordinate k in this.parentMap.tiles.Keys)
-        {
-            Debug.Log(string.Format("{0} {1}", k.x, k.y));
-        }
-        AdventureTile targetTile;
-        XYCoordinate targetCoords = new XYCoordinate(x, y);
-        if (this.parentMap.tiles.TryGetValue(targetCoords, out targetTile))
+        //Debug.Log(string.Format("Trying to walk. {0} {1} {2}", this.parentMap.tiles.Count, x, y));
+        AdventureTile targetTile = this.parentMap.getTileAt(x, y);
+        if (targetTile != null)
         {
             if (targetTile.CanWalk(this))
             {
                 Debug.Log("Can enter tile");
-                this.coordinates = targetCoords;
+                this.x = x;
+                this.y = y;
                 transform.position = new Vector3(x, y, 0f);
                 targetTile.OnEnter();
                 return true;
@@ -58,7 +55,7 @@ public class Actor : MonoBehaviour {
         }
         else
         {
-            Debug.Log("Could not find tile at {0} {1}.")
+            Debug.Log("Could not find tile at {0} {1}.");
             return false;
         }
             
--- Assets/Scripts/Adventure/AdventureMap.cs
+++ Assets/Scripts/Adventure/AdventureMap.cs
@@ -10,49 +10,41 @@ public enum Direction {
 }
 
 public class AdventureMap : MonoBehaviour {
-    public Dictionary<XYCoordinate, AdventureTile> tiles = new Dictionary<XYCoordinate, AdventureTile>();
+    public AdventureTile[,] tiles;
     public PlayerActor player;
     public List<NPActor> npcs;
     public List<AdventureTile> prefabs;
+    protected int maxx, maxy;
     // Use this for initialization
     void Start() {
-        int maxx = 4;
-        int maxy = 4;
-        this.tiles.Clear();
+        this.maxx = 4;
+        this.maxy = 4;
+        this.tiles = new AdventureTile[maxx, maxy];
         int n = prefabs.Count;
         for (int i = 0; i < maxx; i++) {
             for (int j = 0; j < maxy; j++)
             {
-                AdventureTile myTile = Instantiate(prefabs[Random.Range(0, n)], new Vector3(i, j, 0f), Quaternion.identity );
+                AdventureTile myTile = Instantiate(prefabs[Random.Range(0, n)], new Vector3(i, j, 0f), Quaternion.identity);
                 myTile.initTile(i, j, this);
-                this.tiles.Add(new XYCoordinate(i, j), myTile);
+                this.tiles[i, j] = myTile;
             }
         }
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-}
-
-[System.Serializable]
-public class XYCoordinate : IEqualityComparer<XYCoordinate>
-{
-    public int x;
-    public int y;
-    public XYCoordinate(int x, int y)
-    {
-        this.x = x;
-        this.y = y;
     }
 
-    public bool Equals(XYCoordinate one, XYCoordinate other)
-    {
-        return other.x == one.x && one.y == other.y;    
+    // Update is called once per frame
+    void Update() {
+
     }
-    public int GetHashCode(XYCoordinate coord)
+
+    public AdventureTile getTileAt(int x, int y)
     {
-        return coord.x.GetHashCode() ^ coord.y.GetHashCode();
+        if (x >= 0 && y >= 0 && x < this.maxx && y < this.maxy)
+        {
+            return this.tiles[x, y];
+        }
+        else
+        {
+            return null;
+        }
     }
 }
