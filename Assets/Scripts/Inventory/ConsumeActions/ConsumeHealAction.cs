224e86a47f260b3f41e85cb44373ac63c0288284
Robin Ekman <robin.seth.ekman@gmail.com>
2017-10-01 15:19:03 +0200

    Some new Item assets.

---
+++ Assets/Scripts/Inventory/ConsumeActions/ConsumeHealAction.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Item Actions/Consume Heal")]
+public class ConsumeHealAction : ConsumeAction {
+
+
+	public override bool Consume(Item item)
+    {
+        PlayerStats.instance.currentHP += item.restorationValue;
+        return true;
+    }
+}
